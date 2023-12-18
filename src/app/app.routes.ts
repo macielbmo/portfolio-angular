96a55a54989a7ffa8d6794bbc082b5d13fb48654
Maciel Barboza Martins <maciel.martins@MEDIKAAD.lan>
2023-12-18 17:35:13 -0300

    Criação da Page Home e Component Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './modules/portifolio/pages/home/home.component';
+
+// importações
+import { HomeComponent } from './modules/portfolio/pages/home/home.component';
 
 export const routes: Routes = [
     {
